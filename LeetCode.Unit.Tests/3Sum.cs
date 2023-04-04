f18ece38f85cd6394cb14dd2b933787fa1749039
gig\montazeri.fateme <montazeri.fateme@okala.com>
2023-04-04 12:09:52 +0330

    refactor

--- LeetCode.Unit.Tests/3Sum.cs
+++ LeetCode.Unit.Tests/3Sum.cs
@@ -15,14 +15,20 @@ public class ThreeSum
         {
             for (var next = 0; next <= input.Length - 1; next++)
             {
-                if(current == next) continue;
+                if (current == next) continue;
                 var thirdIndex = SearchIndex(input, current, next);
                 if (thirdIndex < 0) continue;
                 var newItem = new List<int> { input[current], input[next], input[thirdIndex] }
                     .OrderBy(ii => ii)
                     .ToList();
-                
-                if (result.All(item => !Enumerable.SequenceEqual(item, newItem)))
+
+                // if (result.All(item => !Enumerable.SequenceEqual(item, newItem)))
+                // {
+                //     result.Add(newItem);
+                // }
+
+                var comparer = new ArrayListComparer();
+                if (!result.Any(item => comparer.Equals(item, newItem))) // item[0] == newItem[0] && item[1] == newItem[1] && item[2] == newItem[2]))
                 {
                     result.Add(newItem);
                 }
@@ -35,9 +41,6 @@ public class ThreeSum
 
     private int SearchIndex(int[] input, int current, int next)
     {
-        //if (next > input.Length - 1) return -1;
-        //if (current == next) return -1;
-
         var value = input[current] + input[next];
         for (var i = 0; i <= input.Length - 1; i++)
         {
@@ -49,5 +52,19 @@ public class ThreeSum
         }
 
         return -1;
+    }
+}
+
+public class ArrayListComparer : EqualityComparer<IList<int>>
+{
+    public override bool Equals(IList<int>? x, IList<int>? y)
+    {
+        if (x == null || y == null) return false;
+        return x[0] == y[0] && x[1] == y[1] && x[2] == y[2];
+    }
+
+    public override int GetHashCode(IList<int> obj)
+    {
+        return obj.GetHashCode();
     }
 }
