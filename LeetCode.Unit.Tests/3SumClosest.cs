19e2d8a72fa73781791919d3e80bbfa90624d72f
gig\montazeri.fateme <montazeri.fateme@okala.com>
2023-04-05 17:26:44 +0330

    refactor - 3 sum closet

--- LeetCode.Unit.Tests/3SumClosest.cs
+++ LeetCode.Unit.Tests/3SumClosest.cs
@@ -5,12 +5,15 @@ namespace LeetCode.Unit.Tests;
 public class ThreeSumClosest
 {
     [Theory]
-    [InlineData(new[] { 0,0,0}, 1 , 0)]
-    [InlineData(new[] { -1,2,1,-4}, 1 , 2)]
-    [InlineData(new[] { 1,1,1,1}, 0 , 3)]
-    public void ThreeSumClosestCalc(int[] input, int target , int output)
+    [InlineData(new[] { 0, 0, 0 }, 1, 0)]
+    [InlineData(new[] { -1, 2, 1, -4 }, 1, 2)]
+    [InlineData(new[] { 1, 1, 1, 1 }, 0, 3)]
+    [InlineData(new[] { 4, 0, 5, -5, 3, 3, 0, -4, -5 }, -2, -2)]
+    [InlineData(new[] { 4, 0, 5, -5, 3, 3, 0, -4, -100 , 50 , 200 , 296 , 80 , 70 , 55 , 34, 22, -1 }, 243, 245)]
+    public void ThreeSumClosestCalc(int[] input, int target, int output)
     {
         var dic = new Dictionary<int, int>();
+      
         Array.Sort(input);
         var n = input.Length;
         for (var i = 0; i < n; i++)
@@ -26,9 +29,10 @@ public class ThreeSumClosest
                 var diff = target > sum ? target - sum : sum - target;
                 if (!dic.ContainsKey(diff))
                 {
-                    dic.Add(diff , sum);
+                    dic.Add(diff, sum);
                 }
-
+                if(sum == target) break;
+                
                 if (sum < target)
                 {
                     left++;
@@ -37,12 +41,11 @@ public class ThreeSumClosest
                 {
                     right--;
                 }
-   
             }
         }
 
-        var properKey = dic.Keys.Min(i=>i);
-        var properSum = dic[properKey];
-        properSum.Should().Be(output);
+        var closetValue = dic.Keys.Min();
+        var closetSum = dic[closetValue];
+        closetSum.Should().Be(output);
     }
 }
